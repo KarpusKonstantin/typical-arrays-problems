af3d0fece44df4eff8d5f6cffd101f7365e8a7de
KarpusKonstantin <60568968+KarpusKonstantin@users.noreply.github.com>
2020-03-06 16:36:58 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) { return 0}
+    array.sort((a,b) =>{return a-b});
+    return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) { return 0}
+    array.sort((a,b) =>{return b-a});
+    return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length === 0) { return 0}
+
+    let a = 0;
+    for (let i = 0; i < array.length; i++) {
+        a = a + array[i];
+    }
+  return a/array.length;
 }
